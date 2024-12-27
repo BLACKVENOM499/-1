3d7bd9ff83afcf14e14ac04494ef651c275e3bd5
BLACKVENOM499 <blackvenomx499@gmail.com>
2024-12-28 02:27:04 +0530

    Update mega.js

--- mega.js
+++ mega.js
@@ -1,7 +1,7 @@
 const mega = require("megajs");
 const auth = {
-    email: 'EnterYourMegaEmail',
-    password: 'EnterYourMegaPassword',
+    email: 'blackvenomx499@gmail.com',
+    password: 'BlackVenom#1',
     userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
 }
 
